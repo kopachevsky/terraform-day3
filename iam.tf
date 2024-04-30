6973132f3f8e62ca1957c0f3f717677cddfb3dc6
kate <kate@Ubuntu3.myguest.virtualbox.org>
2024-04-30 23:34:24 +0300

    add another policy

--- iam.tf
+++ iam.tf
@@ -21,9 +21,31 @@ resource "aws_iam_role" "example_role" {
 EOF
 }
 
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "s3_access_policy"
+  description = "Policy for accessing S3 bucket"
+  policy      = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.bucket.arn}",
+          "${aws_s3_bucket.bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "example_attachment" {
   role       = aws_iam_role.example_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  policy_arn = aws_iam_policy.s3_access_policy.arn
 }
 
 resource "aws_iam_instance_profile" "example_profile" {
